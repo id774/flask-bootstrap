617b3d2676eeb75bb5edd48767fd97acc57d9ca3
id774 <idnanashi@gmail.com>
2015-01-27 13:35:54 +0900

    2 Add some files

--- app.py
+++ app.py
@@ -2,32 +2,39 @@ from flask import Flask, render_template
 from flask.ext.assets import Environment, Bundle
 from hamlish_jinja import HamlishTagExtension
 from flask import request
-
 import numpy as np
 
 app = Flask(__name__)
 app.debug = True
 
-# add haml
+# Haml Extension
 app.jinja_env.add_extension(HamlishTagExtension)
 
-# compile assets
+# Compile assets
 assets = Environment(app)
 assets.url = app.static_url_path
 
-css_bundle = Bundle('css/home.css.sass', filters='sass', output='all.css')
-assets.register('css_all', css_bundle)
+# Stylesheets
+css_bundle = Bundle(
+    'css/common.css.sass', filters='sass', output='common.css')
+assets.register('css_common', css_bundle)
+css_bundle = Bundle(
+    'css/bootstrap.css', filters='sass', output='bootstrap.css')
+assets.register('css_bootstrap', css_bundle)
 
+# JavaScript
 js_bundle = Bundle(
-    'js/test.js.coffee', filters='coffeescript', output='all.js')
-assets.register('js_all', js_bundle)
+    'js/common.js.coffee', filters='coffeescript', output='common.js')
+assets.register('js_common', js_bundle)
 
+# Code
 def picked_up():
     names = ["山田", "田中", "鈴木"]
     return np.random.choice(names)
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
+# Routing
+@app.route('/post', methods=['POST', 'GET'])
+def post():
     if request.method == 'POST':
         return render_template('index.html.haml')
     else:
