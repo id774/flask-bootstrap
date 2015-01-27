95fb4cfa4a0c85a5631c9d2ee5fe5972e0314ca0
id774 <idnanashi@gmail.com>
2015-01-27 15:26:33 +0900

    3 Hello Application

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
-from flask.ext.assets import Environment, Bundle
+from flask import Flask, render_template, request, redirect, url_for
+from flask.ext.assets import Environment
 from hamlish_jinja import HamlishTagExtension
-from flask import request
 import numpy as np
 
 app = Flask(__name__)
@@ -14,36 +13,28 @@ app.jinja_env.add_extension(HamlishTagExtension)
 assets = Environment(app)
 assets.url = app.static_url_path
 
-# Stylesheets
-css_bundle = Bundle(
-    'css/common.css.sass', filters='sass', output='common.css')
-assets.register('css_common', css_bundle)
-css_bundle = Bundle(
-    'css/bootstrap.css', filters='sass', output='bootstrap.css')
-assets.register('css_bootstrap', css_bundle)
-
-# JavaScript
-js_bundle = Bundle(
-    'js/common.js.coffee', filters='coffeescript', output='common.js')
-assets.register('js_common', js_bundle)
-
-# Code
+# Main
 def picked_up():
-    names = ["山田", "田中", "鈴木"]
-    return np.random.choice(names)
+    messages = [
+        "こんにちは、あなたの名前を入力してください",
+        "やあ！お名前は何ですか？",
+        "あなたの名前を教えてね"
+    ]
+    return np.random.choice(messages)
 
 # Routing
+@app.route('/')
+def index():
+    message = picked_up()
+    return render_template('index.html', message=message)
+
 @app.route('/post', methods=['POST', 'GET'])
 def post():
     if request.method == 'POST':
-        return render_template('index.html.haml')
+        name = request.form['name']
+        return render_template('index.html', name=name)
     else:
-        name = picked_up()
-        return render_template('result.html.haml', name=name)
-
-@app.route('/')
-def hello_word():
-    return render_template('index.html.haml')
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
