fbd3aa95928db345aa8f00cf041a65dbb998d4bb
id774 <idnanashi@gmail.com>
2015-01-29 20:10:32 +0900

    9 Simple Hello

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, redirect, url_for
-from flask.ext.assets import Environment
-from hamlish_jinja import HamlishTagExtension
 import numpy as np
 
 app = Flask(__name__)
-
-# Haml Extension
-app.jinja_env.add_extension(HamlishTagExtension)
-
-# Compile assets
-assets = Environment(app)
-assets.url = app.static_url_path
 
 # Main
 def picked_up():
@@ -26,16 +17,21 @@ def picked_up():
 # Routing
 @app.route('/')
 def index():
+    title = "ようこそ"
     message = picked_up()
-    return render_template('index.html', message=message)
+    return render_template('index.html',
+                           message=message, title=title)
 
 @app.route('/post', methods=['POST', 'GET'])
 def post():
+    title = "こんにちは"
     if request.method == 'POST':
         name = request.form['name']
-        return render_template('index.html', name=name)
+        return render_template('index.html',
+                               name=name, title=title)
     else:
         return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0')
